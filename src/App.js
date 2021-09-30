1cf83c1de0503e7399a18b7a09c9f42aae8772b5
Tejas Kothari <tejas23682320@gmail.com>
2021-09-30 16:44:19 +0800

    Pool info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import Header from "./components/Header";
+import USDC_image from "./assets/USDC.png";
 
 function App() {
   return (
-    <div className="App">
+    <div className="min-h-screen bg-primary font-primary">
       <Header />
+      <div className="flex justify-between items-center px-5">
+        <div className="flex justify-between items-center">
+          <img src={USDC_image} className="w-16" />
+          <div className="flex flex-col">
+            <div className="flex-1">USDC Coin</div>
+            <div className="flex-1">POOLS // ETHEREUM // USDC POOL</div>
+          </div>
+        </div>
+        <div className="rounded-full border-2 border-secondary text-2xl text-secondary px-16 py-2 font-black cursor-pointer">
+          Deposit
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import { ReactComponent as SettingsIcon } from "../assets/001-settings.svg";
 
 export default function Header() {
   return (
-    <div className="h-16 px-5 flex justify-between items-center font-primary text-secondary">
+    <div className="h-16 px-5 flex justify-between items-center text-secondary">
       <div className="text-3xl font-black">DeFi PowerBall</div>
       <div className="flex justify-between items-center">
-        <div className="rounded-full border-2 border-secondary px-6 py-1 text-xs font-bold cursor-pointer">
+        <div className="rounded-full border-2 border-secondary px-6 py-1 text-xs font-black cursor-pointer">
           Connect Wallet
         </div>
         <SettingsIcon className="w-5 ml-3 cursor-pointer fill-secondary" />
