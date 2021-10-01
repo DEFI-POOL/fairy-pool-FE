a7971e53d4e1aa8259b7bf9a2b7f0d385892236d
Tejas Kothari <tejas23682320@gmail.com>
2021-10-01 22:07:47 +0800

    Pool prize banner

--- src/App.js
+++ src/App.js
@@ -6,27 +6,38 @@ function App() {
   return (
     <div className="min-h-screen bg-primary font-primary">
       <Header />
-      <div className="flex justify-between items-center px-5">
-        <div className="flex items-center">
-          <img src={USDC_image} className="w-16" />
-          <div className="ml-8">
-            <div className="flex items-center">
-              <div className="text-3xl font-bold text-secondary">USDC Coin</div>
-              <div className="rounded-full bg-blue-900 text-accent text-xs font-black ml-2 px-2 py-0.5">
-                Weekly prize
+      <div className="px-5">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
+            <img src={USDC_image} className="w-14" />
+            <div className="ml-8">
+              <div className="flex items-center">
+                <div className="text-3xl font-bold text-secondary">
+                  USDC Coin
+                </div>
+                <div className="rounded-full bg-blue-900 text-accent text-xs font-black ml-2 px-2 py-0.5">
+                  Weekly prize
+                </div>
+              </div>
+              <div className="text-secondary text-sm">
+                POOLS
+                <Chevron className="inline-block w-2.5 fill-secondary align-baseline mx-1" />
+                ETHEREUM
+                <Chevron className="inline-block w-2.5 fill-secondary align-baseline mx-1" />
+                USDC POOL
               </div>
             </div>
-            <div className="text-secondary text-sm">
-              POOLS
-              <Chevron className="inline-block w-2.5 fill-secondary align-baseline mx-1" />
-              ETHEREUM
-              <Chevron className="inline-block w-2.5 fill-secondary align-baseline mx-1" />
-              USDC POOL
-            </div>
+          </div>
+          <div className="rounded-full border-2 border-accent text-2xl text-accent px-16 py-2 font-black cursor-pointer">
+            Deposit
           </div>
         </div>
-        <div className="rounded-full border-2 border-accent text-2xl text-accent px-16 py-2 font-black cursor-pointer">
-          Deposit
+        <div className="mt-8 rounded-lg bg-gradient-to-r from-pink-600 to-purple-800 flex justify-between items-center px-14 py-8">
+          <div>
+            <div className="text-secondary">Prize #96</div>
+            <div className="text-white text-5xl font-bold">$69,420</div>
+          </div>
+          <div className="text-secondary text-lg">Will be awarded</div>
         </div>
       </div>
     </div>
