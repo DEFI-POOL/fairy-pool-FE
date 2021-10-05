ef1777403534b9d379c9d2bb27e61f6d149b2e79
Tejas Kothari <tejas23682320@gmail.com>
2021-10-05 18:52:33 +0800

    Web3 connect wallet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import { ReactComponent as SettingsIcon } from "../assets/001-settings.svg";
 import logo from "../assets/logo.png";
+import Web3 from "web3";
 
 export default function Header() {
+  const [ethAdd, setEthAdd] = useState(null);
+
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      const userAdd = await window.ethereum.send("eth_requestAccounts");
+      setEthAdd(userAdd.result);
+      const web3 = new Web3(window.ethereum);
+    }
+  };
+
   return (
     <div className="h-16 px-5 flex justify-between items-center pt-8">
       <div className="flex items-center">
@@ -18,9 +30,16 @@ export default function Header() {
         </div>
       </div>
       <div className="flex justify-between items-center">
-        <div className="rounded-full border-2 border-accent px-6 py-1 text-sm text-accent font-semibold cursor-pointer hover:bg-accent hover:text-primary">
-          Connect Wallet
-        </div>
+        {ethAdd ? (
+          <div className="text-sm text-secondary font-semibold">{ethAdd}</div>
+        ) : (
+          <div
+            onClick={connectWallet}
+            className="rounded-full border-2 border-accent px-6 py-1 text-sm text-accent font-semibold cursor-pointer hover:bg-accent hover:text-primary"
+          >
+            Connect Wallet
+          </div>
+        )}
         <SettingsIcon className="w-5 ml-3 cursor-pointer fill-accent" />
       </div>
     </div>
