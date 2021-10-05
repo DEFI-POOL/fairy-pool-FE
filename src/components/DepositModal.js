ddef3e9998d6384a0d7a1095b258c77148d93ed4
Tejas Kothari <tejas23682320@gmail.com>
2021-10-05 23:47:49 +0800

    CSS edits

--- src/components/DepositModal.js
+++ src/components/DepositModal.js
@@ -11,16 +11,16 @@ export default function DepositModal({ close }) {
         className="rounded-lg bg-gradient-to-br from-purple-900 via-purple-800 to-purple-600 shadow-2xl py-10 px-16 flex flex-col items-center"
       >
         <img src={USDC_image} className="w-14" />
-        <div className="text-white font-primary font-semibold text-xl mt-2">
+        <div className="text-white font-semibold text-xl mt-2">
           Deposit into the USDC Pool
         </div>
-        <div className="w-full mt-16 font-primary text-accent">Amount</div>
+        <div className="w-full mt-16 text-accent">Amount</div>
         <input
           type="number"
           autoFocus
-          className="outline-none ring rounded-full pl-2 py-1 bg-purple-900 text-white font-primary text-3xl mt-2"
+          className="outline-none ring rounded-full pl-4 py-1 bg-purple-900 text-white text-3xl mt-2"
         />
-        <div className="rounded-full border-2 border-accent px-8 py-1 text-xl text-accent font-semibold font-primary cursor-pointer hover:bg-accent hover:text-primary mt-8">
+        <div className="rounded-full border-2 border-accent px-8 py-1 text-xl text-accent font-semibold cursor-pointer hover:bg-accent hover:text-primary mt-8">
           Continue
         </div>
       </div>
