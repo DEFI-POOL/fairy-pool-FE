2229c4af68f24f273f9ec8c831f3575b62497832
george-shammar <george.gbenle@outlook.com>
2021-10-07 19:16:57 +0000

    Implement connectWallet

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -1,5 +1,17 @@
 export const connectWallet = async () => {
     if (window.ethereum) {
-        
+        try {
+            const addressArray = await window.ethereum.request({
+              method: "eth_requestAccounts",
+            });
+            const obj = {
+              status: "You're all set!!! Deposit! Win pool interest! Never Lose!",
+              address: addressArray[0],
+            };
+            return obj;
+          }
+
+
+
     }
 }
