25ab5f7121758e5c443ccf7c9a1ecc7a48d6f9f8
george-shammar <george.gbenle@outlook.com>
2021-10-07 19:46:56 +0000

    Create getcurrentWalletConnected

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -38,5 +38,49 @@ export const connectWallet = async () => {
 // For an already connected wallet upon browser refresh
 
 export const getCurrentWalletConnected = async () => {
-  
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (addressArray.length > 0) {
+        return {
+          address: addressArray[0],
+          status: "You're all set!!! Deposit! Win pool interest! Never Lose!",
+        };
+      } else {
+        return {
+          address: "",
+          status: "🦊 Connect to Metamask using the top right button.",
+        };
+      }
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href={`https://metamask.io/download.html`}>
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+
+
+
+
+
+
 }
