ae3d76ec2f30a7f214f6f736da4a2e737934ac35
george-shammar <george.gbenle@outlook.com>
2021-10-07 19:57:50 +0000

    Remove old code

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,7 @@ import logo from "../assets/logo.png";
 import Web3 from "web3";
 
 export default function Header() {
-  const [ethAdd, setEthAdd] = useState(null);
-
-  const connectWallet = async () => {
-    if (window.ethereum) {
-      const userAdd = await window.ethereum.send("eth_requestAccounts");
-      setEthAdd(userAdd.result);
-      const web3 = new Web3(window.ethereum);
-    }
-  };
+  
 
   return (
     <div className="h-16 px-5 flex justify-between items-center pt-8">
@@ -30,16 +22,13 @@ export default function Header() {
         </div>
       </div>
       <div className="flex justify-between items-center">
-        {ethAdd ? (
-          <div className="text-sm text-secondary font-semibold">{ethAdd}</div>
-        ) : (
+          <div className="text-sm text-secondary font-semibold"></div>
+        
           <div
-            onClick={connectWallet}
             className="rounded-full border-2 border-accent px-6 py-1 text-sm text-accent font-semibold cursor-pointer hover:bg-accent hover:text-primary"
           >
             Connect Wallet
           </div>
-        )}
         <SettingsIcon className="w-5 ml-3 cursor-pointer fill-accent" />
       </div>
     </div>
