3dd83da4b7a13211c2876e3d1a145951f2815fc9
george-shammar <george.gbenle@outlook.com>
2021-10-12 08:01:36 +0000

    Create web3 instance

--- src/utils/compound_ETH.js
+++ src/utils/compound_ETH.js
@@ -1,6 +1,10 @@
-const Web3 = require('web3');
+import Web3 from 'web3';
+const web3 = new Web3(window.web3.currentProvider);
 
-const web3 = new Web3('');
+export default web3;
+// const Web3 = require('web3');
+
+// const web3 = new Web3('');
 
 // Your Ethereum wallet private key
 const privateKey = '';
